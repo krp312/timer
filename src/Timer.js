77dafbfc3f88b8dddcd50d9a4d381f9c9552b529
krp312 <krp312@me.com>
2017-08-12 22:48:47 -0400

    finish ms to H:M:S converter

--- src/Timer.js
+++ src/Timer.js
@@ -15,17 +15,35 @@ export default function Timer() {
   );
 }
 
-const userInput = 3000;
-const startTime = new Date().getTime();
-
-const callback = () => {
-  let currentTime = new Date().getTime();
-
-  if (currentTime - startTime > userInput) {
-    console.log('done');
-  }
-
-  document.title = (currentTime - startTime) / 1000;
+const convertHMStoMilliseconds = (hours, minutes, seconds) => {
+  const hoursToMinutes = hours * 60;
+  const minutesToSeconds = (hoursToMinutes + minutes) * 60;
+  const milliseconds = (minutesToSeconds + seconds) * 1000;
+  return milliseconds;
 };
 
-window.setInterval(callback, 1000);
+const convertMillisecondsToHMS = milliseconds => {
+
+};
+
+const userInput = convertHMStoMilliseconds(1, 30, 900);
+const startTime = new Date().getTime();
+const counter = () => {
+  let currentTime = new Date().getTime();
+
+  if (currentTime - startTime >= userInput) {
+    console.log('timer done');
+    clearInterval(timer);
+  }
+  
+  document.title = ((startTime + userInput) - currentTime); // Math.ceil seems to work well, even when going into negatives
+};
+
+const timer = window.setInterval(counter, 1000);
+
+const countdownTimer = () => {
+  counter(); // needs to happen to fire first iteration of itself, otherwise there will be a delay that's the second arg of setInterval
+  timer;
+};
+
+countdownTimer();
