9c305f71f32e983544000cbab297234c02a5135d
krp312 <krp312@me.com>
2017-08-13 00:09:16 -0400

    add display of time counting down

--- src/Timer.js
+++ src/Timer.js
@@ -1,10 +1,5 @@
 import React from 'react';
 
-// start a countdown
-// store it in state
-// display state to UI
-// when user clicks stop, the time is saved somewhere
-
 export default function Timer() {
   return (
     <form action="">
@@ -22,33 +17,28 @@ const convertHMStoMilliseconds = (hours, minutes, seconds) => {
   return milliseconds;
 };
 
-const convertMillisecondsToHMS = milliseconds => {
-  const hours = Math.floor(milliseconds/1000/60/60);
-  const remainingMinsInMilliseconds = milliseconds % 3600000;
-  const remainingMinutes = remainingMinsInMilliseconds/1000/60;
-  const remainingSecondsInMilliseconds = remainingMinsInMilliseconds % 60000;
-  const remainingSeconds = remainingSecondsInMilliseconds/1000;
-
-  // console.log(hours, remainingMinutes, remainingSeconds);
-};
-
-const userInput = convertHMStoMilliseconds(1, 30, 900);
+const userInput = convertHMStoMilliseconds(0, 0, 3);
 const startTime = new Date().getTime();
 const counter = () => {
-  let currentTime = new Date().getTime();
+  const currentTime = new Date().getTime();
 
   if (currentTime - startTime >= userInput) {
     console.log('timer done');
     clearInterval(timer);
   }
-  convertMillisecondsToHMS(((startTime + userInput) - currentTime));
-  document.title = ((startTime + userInput) - currentTime); // Math.ceil seems to work well, even when going into negatives
+
+  const milliseconds = ((startTime + userInput) - currentTime);
+  const date = new Date(null);
+  date.setMilliseconds(milliseconds);
+  const result = date.toISOString().substring(11, 19);
+
+  document.title = result;
 };
 
 const timer = window.setInterval(counter, 1000);
 
 const countdownTimer = () => {
-  counter(); // needs to happen to fire first iteration of itself, otherwise there will be a delay that's the second arg of setInterval
+  counter();
   timer;
 };
 
